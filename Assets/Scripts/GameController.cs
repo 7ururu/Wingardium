c9a23df8b2019b5671d90845de02a1b6a0a7635a
Dmitry Koblyk <koblyk.dmitry@gmail.com>
2020-02-01 13:51:04 +0200

    first steps

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public sealed class GameController : MonoBehaviour {
+
+    [SerializeField]
+    private Rigidbody2D _target;
+
+    [SerializeField]
+    private float _force;
+
+    private void Update() {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            _target.AddForce(new Vector2(0f, _force * 2f));
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            _target.AddForce(new Vector2(0f, -_force));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            _target.AddForce(new Vector2(-_force, 0f));
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            _target.AddForce(new Vector2(_force, 0f));
+        }
+
+    }
+}
